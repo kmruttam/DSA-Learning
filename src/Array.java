02db85eb7c3fc39af876763729190a0d596f2660
Uttam <71337428+kmruttam@users.noreply.github.com>
2024-09-30 20:18:59 +0530

    added array and linked list

---
+++ src/Array.java
@@ -0,0 +1,50 @@
+public class Array {
+    private int[] items;
+    private int size = 0;
+
+    public Array(int length) {
+        items = new int[length];
+    }
+
+    public int indexOf(int item) {
+        for (int i = 0; i < size; i++) {
+            if (item == items[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void insert(int item) {
+        if (items.length == size) {
+            int[] newItems = new int[size * 2];
+            for (int i = 0; i < size; i++) {
+                newItems[i] = items[i];
+            }
+            items = newItems;
+        }
+        items[size++] = item;
+    }
+
+    public void print() {
+        System.out.print("[");
+        for (int i = 0; i < size; i++) {
+            if (i == size - 1) {
+                System.out.print(items[i] + "");
+            } else {
+                System.out.print(items[i] + ", ");
+            }
+        }
+        System.out.println("]");
+    }
+
+    public void removeAt(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = index; i < size; i++) {
+            items[i] = items[i + 1];
+        }
+        size--;
+    }
+}
---
+++ src/LinkedList.java
@@ -0,0 +1,168 @@
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+  private class Node {
+    private int value;
+    private Node next;
+
+    public Node(int value) {
+      this.value = value;
+    }
+  }
+
+  private Node first;
+  private Node last;
+  private int size;
+
+  private boolean isEmpty() {
+    return first == null;
+  }
+
+  public void addLast(int item) {
+    var node = new Node(item);
+    if (isEmpty()) {
+      first = last = node;
+    } else {
+      last.next = node;
+      last = node;
+    }
+    size++;
+  }
+
+  public void addFirst(int item) {
+    var node = new Node(item);
+
+    if (isEmpty()) {
+      first = last = node;
+    } else {
+      node.next = first;
+      first = node;
+    }
+    size++;
+  }
+
+  public void removeFirst() {
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
+    if (first == last) {
+      first = last = null;
+    } else {
+      Node second = first.next;
+      first.next = null;
+      first = second;
+    }
+
+    size--;
+  }
+
+  public void removeLast() {
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
+    if (first == last) {
+      first = last = null;
+    } else {
+      Node previous = getPrevious(last);
+      last = previous;
+      last.next = null;
+    }
+    size--;
+  }
+
+  private Node getPrevious(Node node) {
+    Node current = first;
+    while (current != null) {
+      if (current.next == node) {
+        return current;
+      }
+      current = current.next;
+    }
+    return null;
+  }
+
+  public boolean contains(int item) {
+    return indexOf(item) != -1;
+  }
+
+  public int indexOf(int item) {
+    int index = 0;
+    Node current = first;
+    while (current != null) {
+      if (current.value == item) {
+        return index;
+      }
+      current = current.next;
+      index++;
+    }
+    return -1;
+  }
+
+  public void print() {
+    Node current = first;
+    System.out.print("[");
+    while (current != null) {
+      System.out.print(current.value);
+      if (current == last) {
+        System.out.print("");
+      } else {
+        System.out.print(", ");
+      }
+      current = current.next;
+    }
+    System.out.println("]");
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public int[] toArray() {
+    int[] items = new int[size];
+    Node current = first;
+    int index = 0;
+    while (current != null) {
+      items[index++] = current.value;
+      current = current.next;
+    }
+    return items;
+  }
+
+  public void reverse() {
+    if (isEmpty()) {
+      return;
+    }
+
+    Node previous = first;
+    Node current = first.next;
+    while (current != null) {
+      Node next = current.next;
+      current.next = previous;
+      previous = current;
+      current = next;
+    }
+
+    last = first;
+    last.next = null;
+    first = previous;
+  }
+
+  public int getKthFromTheEnd(int k) {
+    if(isEmpty() || k < 1){
+      throw new IllegalArgumentException();
+    }
+    Node a = first;
+    Node b = first;
+    for(int i = 0; i < k -1; i++){
+      b = b.next;
+      if(b == null){
+        throw new IllegalArgumentException();
+      }
+    }
+    while(b != last) {
+      a = a.next;
+      b = b.next;
+    }
+    return a.value;
+  }
+}
