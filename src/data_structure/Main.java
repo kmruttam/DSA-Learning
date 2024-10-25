ebec46bd995277c7c6f1418bb3e44037fdefc8b4
Uttam <71337428+kmruttam@users.noreply.github.com>
2024-10-25 14:59:23 +0530

    Weighted Graph

--- src/data_structure/Main.java
+++ src/data_structure/Main.java
@@ -2,12 +2,6 @@ package data_structure;
 
 public class Main {
     public static void main(String[] args) {
-        Trie trie = new Trie();
-        trie.insert("car");
-        trie.insert("card");
-        trie.insert("care");
-        trie.insert("careful");
-        trie.insert("egg");
-        System.out.println(trie.findWords("care"));
+
     }
 }
